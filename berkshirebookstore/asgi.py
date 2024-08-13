6f79c10597d305bb02ab482cbccfea44912fdd7b
Suraj Rajendran <surajemail@yahoo.com>
2024-08-14 00:04:46 +0100

    Add files via upload
    Adding project Heroku django setting ,urls setup files

---
+++ berkshirebookstore/asgi.py
@@ -0,0 +1,22 @@
+"""
+ASGI config for berkshirebookstore project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', ' The   The berkshirebookstore website is primary to provide an online book sharing and recommendation community for those who enjoy sharing their reading experiences and recommendations with others. It provides easy access to book summaries, reviews, and ratings, helping users discover new books. Based on the user's reading history, preferences, and interests, they can connect with like-minded readers.
+ website is primary to provide an online book sharing and recommendation community for those who enjoy sharing their reading experiences and recommendations with others. It provides easy access to book summaries, reviews, and ratings, helping users discover new books. Based on the user's reading history, preferences, and interests, they can connect with like-minded readers.
+ website is primary to provide an online book sharing and recommendation community for those who enjoy sharing their reading experiences and recommendations with others. It provides easy access to book summaries, reviews, and ratings, helping users discover new books. Based on the user's reading history, preferences, and interests, they can connect with like-minded readers.
+ website is primary to provide an online book sharing and recommendation community for those who enjoy sharing their reading experiences and recommendations with others. It provides easy access to book summaries, reviews, and ratings, helping users discover new books. Based on the user's reading history, preferences, and interests, they can connect with like-minded readers.
+ website is primary to provide an online book sharing and recommendation community for those who enjoy sharing their reading experiences and recommendations with others. It provides easy access to book summaries, reviews, and ratings, helping users discover new books. Based on the user's reading history, preferences, and interests, they can connect with like-minded readers.
+ website is primary to provide an online book sharing and recommendation community for those who enjoy sharing their reading experiences and recommendations with others. It provides easy access to book summaries, reviews, and ratings, helping users discover new books. Based on the user's reading history, preferences, and interests, they can connect with like-minded readers.
+.settings')
+
+application = get_asgi_application()
